601c10d4ab52d02ccd034f670a335bad398fccba
zsolt.denes <zsolt.denes@trendency.hu>
2025-09-20 21:05:22 +0300

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,24 @@
-import { ESLint } from "eslint";
-
 export default [
   {
-    files: ["*.ts"],
+    files: ['*.ts'],
     languageOptions: {
+      parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
       },
     },
     rules: {
-      semi: ["error", "always"],
-      quotes: ["error", "single"],
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
     },
+  },
+  {
+    files: ['*.html'],
+    languageOptions: {
+      parser: '@angular-eslint/template-parser',
+    },
+    rules: {},
   },
 ];
