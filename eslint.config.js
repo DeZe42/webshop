faf2d061c55053bd3723735c78acb179e3761fc4
zsolt.denes <zsolt.denes@trendency.hu>
2025-09-20 21:09:34 +0300

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,44 @@
-import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default [
-  {
-    files: ['*.ts'],
-    languageOptions: {
+export default {
+  root: true,
+  ignorePatterns: ['src/main.server.ts'],
+
+  overrides: [
+    {
+      files: ['*.ts'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
         project: './tsconfig.json',
         tsconfigRootDir: __dirname,
         sourceType: 'module',
+        createDefaultProgram: true
       },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@angular-eslint/recommended',
+        'plugin:@angular-eslint/template/process-inline-templates',
+        'plugin:prettier/recommended'
+      ],
+      plugins: ['@typescript-eslint'],
+      rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        '@typescript-eslint/no-explicit-any': 'off'
+      }
     },
-    rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-    },
-  },
-  {
-    files: ['*.html'],
-    languageOptions: {
+    {
+      files: ['*.html'],
       parser: '@angular-eslint/template-parser',
-    },
-    rules: {},
-  },
-];
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {
+        '@angular-eslint/template/prefer-self-closing-tags': ['error']
+      }
+    }
+  ]
+};
