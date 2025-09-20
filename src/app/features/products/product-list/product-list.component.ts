f84b08a48908c7773e0f4d62a084e90dc669e210
zsolt.denes <zsolt.denes@trendency.hu>
2025-09-20 14:47:52 +0300

    search for product list

--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -1,23 +1,42 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
-import {Product} from '../product.interface';
-import {ProductsService} from '../../../core/services/products.service';
-import {CartService} from '../../../core/services/cart.service';
-import {Router} from '@angular/router';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Product } from '../product.interface';
+import { ProductsService } from '../../../core/services/products.service';
+import { CartService } from '../../../core/services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
   private _productsService = inject(ProductsService);
   private _cart = inject(CartService);
   private _router = inject(Router);
+
   products = signal<Product[]>([]);
+  searchTerm = signal('');
+  selectedCategory = signal<string | null>(null);
+
+  filteredProducts = computed(() =>
+    this.products().filter(product => {
+      const matchesName = product.name.toLowerCase().includes(this.searchTerm().toLowerCase());
+      const matchesCategory = this.selectedCategory() ? product.type === this.selectedCategory() : true;
+      return matchesName && matchesCategory;
+    })
+  );
 
   ngOnInit() {
     this._productsService.fetchAll().subscribe(data => this.products.set(data));
+  }
+
+  updateSearch(term: string) {
+    this.searchTerm.set(term);
+  }
+
+  updateCategory(category: string | null) {
+    this.selectedCategory.set(category);
   }
 
   addToCart(product: Product) {
