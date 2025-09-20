52725d652c74a8fe8be78fdaf414ae8ad8f5cdbb
zsolt.denes <zsolt.denes@trendency.hu>
2025-09-20 21:10:39 +0300

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,41 +4,40 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default {
-  root: true,
-  ignorePatterns: ['src/main.server.ts'],
-
-  overrides: [
-    {
-      files: ['*.ts'],
+export default [
+  {
+    files: ['*.ts'],
+    languageOptions: {
       parser: '@typescript-eslint/parser',
       parserOptions: {
         project: './tsconfig.json',
         tsconfigRootDir: __dirname,
         sourceType: 'module',
         createDefaultProgram: true
-      },
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@angular-eslint/recommended',
-        'plugin:@angular-eslint/template/process-inline-templates',
-        'plugin:prettier/recommended'
-      ],
-      plugins: ['@typescript-eslint'],
-      rules: {
-        semi: ['error', 'always'],
-        quotes: ['error', 'single'],
-        '@typescript-eslint/no-explicit-any': 'off'
       }
     },
-    {
-      files: ['*.html'],
-      parser: '@angular-eslint/template-parser',
-      extends: ['plugin:@angular-eslint/template/recommended'],
-      rules: {
-        '@angular-eslint/template/prefer-self-closing-tags': ['error']
-      }
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@angular-eslint/recommended',
+      'plugin:@angular-eslint/template/process-inline-templates',
+      'plugin:prettier/recommended'
+    ],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      '@typescript-eslint/no-explicit-any': 'off'
     }
-  ]
-};
+  },
+  {
+    files: ['*.html'],
+    languageOptions: {
+      parser: '@angular-eslint/template-parser'
+    },
+    extends: ['plugin:@angular-eslint/template/recommended'],
+    rules: {
+      '@angular-eslint/template/prefer-self-closing-tags': ['error']
+    }
+  }
+];
